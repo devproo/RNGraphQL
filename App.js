edfb758938e4f1040393d3d9496a960ce90fd81d
ipeerless <ipeerless0@gmail.com>
2022-11-17 22:43:15 +0200

    react grapgql

--- App.js
+++ App.js
@@ -1,40 +1,98 @@
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+import Pokemon from "./src/components/Pokemon";
+import getRandomInt from "./src/helpers/getRandomInt";
+import { View, Text, ActivityIndicator } from "react-native";
+import { ApolloProvider, Query } from "react-apollo";
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
+export const AppContext = React.createContext({ data: { pokemon: null } });
+const client = new ApolloClient({
+  uri: "192.168.1.5:4000/graphql",
+});
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu",
 });
 
 export default class App extends Component {
+  state = {
+    query: null,
+  };
+  componentDidMount() {
+    const query = this.getQuery();
+    this.setState({
+      query,
+    });
+  }
+  getQuery = () => {
+    const randomID = getRandomInt(1, 807);
+    return `
+        query GetPokemonById {
+          pokemon(id: ${randomID}) {
+            id,
+            name,
+            desc,
+            pic,
+            types {
+              id,
+              name
+            }
+          }
+        }
+      `;
+  };
+  onGetNewPokemon = () => {
+    const query = this.getQuery();
+    this.setState({
+      query,
+    });
+  };
   render() {
+    const { query } = this.state;
+    if (!query) return null;
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <ApolloProvider client={client}>
+        <Query
+          query={gql`
+            ${query}
+          `}
+        >
+          {({ loading, error, data }) => {
+            if (loading || error)
+              return <ActivityIndicator size="large" color="#0000ff" />;
+            return (
+              <AppContext.Provider
+                value={{ ...data.pokemon, onPress: this.onGetNewPokemon }}
+                style={styles.container}
+              >
+                <Pokemon />
+              </AppContext.Provider>
+            );
+          }}
+        </Query>
+      </ApolloProvider>
     );
   }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
+    textAlign: "center",
     margin: 10,
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
+    textAlign: "center",
+    color: "#333333",
     marginBottom: 5,
   },
 });
---
+++ src/helpers.js
@@ -0,0 +1,8 @@
+// src/helpers/getRandomInt.js
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+export default getRandomInt;
